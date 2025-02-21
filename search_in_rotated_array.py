ed23f83f61917ecb50e82d9bb96b19ab5ba3529b
harsh <harshway08@gmail.com>
2025-02-21 15:24:30 +0530

    search in rotated arr

--- search_in_rotated_array.py
+++ search_in_rotated_array.py
@@ -1,3 +1,32 @@
 def search(nums, target):
-    # Implement your solution here
-    pass
+    
+    left, right = 0, len(nums) - 1
+
+    index = -1
+
+    while left <= right:
+       mid = (left + right) // 2
+       
+       if nums[mid] == target:
+           return mid
+       
+       if nums[left] <= nums[mid]:
+           if nums[left] <= target < nums[mid]:
+               right = mid-1
+           else:
+               left = mid + 1
+       else:
+           if nums[mid] < target <= nums[right]:
+               left = mid + 1
+           else:
+               right = mid - 1
+
+    return -1
+
+
+
+print(search([4,5,6,7,0,1,2,3], 0))
+
+
+
+
